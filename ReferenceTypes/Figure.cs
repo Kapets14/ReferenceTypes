b9409dc38b1918fa6cef9287fb3707fd47363884
исп-43 <-43@LAB24-13>
2021-11-22 15:38:09 +0300

    Добавьте файлы проекта.

---
+++ ReferenceTypes/Figure.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReferenceTypes
+{
+    public abstract class Figure
+    {
+        //Создаём начальные точки
+        public int X { get; set; }
+        public int Y { get; set; }
+        public abstract string Draw();
+
+        //Создаём канструктор чтоб в Х и У загрузить значения
+        public Figure(int x, int y) 
+        {
+            X = x;
+            Y = y;
+        }
+
+    }
+}
